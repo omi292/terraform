65971b96574160433d062c585e63579c92c73d33
Omkar Barge <omkarbarge66@gmail.com>
2026-06-25 22:50:15 +0530

    lock

--- Day-4-state-locking/backend.tf
+++ Day-4-state-locking/backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "newwwwwwww-pytho"
     key    = "terraform.tfstate"
     region = "ap-south-1"
+    use_lockfile = true # To use s3 native locking applied to verion 1.19 above
   }
 }
