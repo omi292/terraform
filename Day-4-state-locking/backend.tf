433112fb1e6df3738905b74f166928e484d159c1
Omkar Barge <omkarbarge66@gmail.com>
2026-06-25 23:03:08 +0530

    com

--- Day-4-state-locking/backend.tf
+++ Day-4-state-locking/backend.tf
@@ -3,6 +3,8 @@ terraform {
     bucket = "newwwwwwww-pytho"
     key    = "terraform.tfstate"
     region = "ap-south-1"
-    use_lockfile = true # To use s3 native locking applied to verion 1.19 above
+    #use_lockfile = true # To use s3 native locking applied to verion 1.19 above
+    dynamodb_table = "nikhil" # Any version we can use dynamodb locking
+    encrypt = true
   }
 }
